8306be6a9025f80a9609e466e0e433a4546602e9
Ockay Martin <martin.ockay@uniqa.at>
2024-03-08 17:39:02 +0100

    CI/CD adaptation + provisional test

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,5 +1,10 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000')
+    cy.get('[data-type="cookie-banner-allow-btn"]').click()
+    cy.get('[data-type="product-cart-button-0"]').click()
+    cy.get('[data-type="navigate-to-cart"]').click()
+    cy.get('[data-type="cart-single-item-total-price-0"]').should('have.text','15.99 €')
+
   })
 })
